5bbe30bb9d99a2e55363bd5901fd6491c43a303a
akhila <akhilaanilkumar104@gmail.com>
2024-07-13 20:28:22 +0530

    five

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,6 +4,6 @@ class ColorApp {
   static const bgcolor = Colors.red;
   static const scafbgcolor = Color.fromARGB(255, 8, 59, 49);
   
-  static const bgcolor = Color.fromARGB(255, 12, 59, 2);
+  // static const bgcolor = Color.fromARGB(255, 12, 59, 2);
   static const mainText = Colors.white;
 }
