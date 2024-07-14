963cebc21a02a8ffab44e60b5ca42ff329499f04
akhila <akhilaanilkumar104@gmail.com>
2024-07-14 22:08:08 +0530

    signup

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -10,37 +11,82 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return
-    SafeArea(child:  Scaffold(
-      
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Text("SIGNUP HERE",style:  TextStyle(color: Colors.white,fontWeight: FontWeight.w500,fontSize: 40),),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(19.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(19.0),
+                  child: Text(
+                    "SIGNUP HERE",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(19.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter email address",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(19.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "phonenumber",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(19.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Location",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(19.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Container(
+                  width: 120,
+                  height: 50,
+                  child: SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Signup",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-             TextFormField(decoration: InputDecoration(hintText: "Enter name",),),
-            TextFormField(decoration: InputDecoration(hintText: "Enter email address"),),
-             TextFormField(decoration: InputDecoration(hintText: "phonenumber"),),
-              TextFormField(decoration: InputDecoration(hintText: "Location"),),
-            
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: TextButton(onPressed: (){}, child: Text("Signup")),
-            )
-
-            
-            
-                
-                
-          ],
+          ),
         ),
       ),
-
-
-    ),);
+    );
   }
 }
